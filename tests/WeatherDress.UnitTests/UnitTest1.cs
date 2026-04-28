31613b41043fc66162627ee279d91c48ce43b27a
Adam-0704 <adamchino8@gmail.com>
2026-04-28 11:37:12 +0200

    refactor:  Fix unit tests to match Dataforsyningen API response format and update FakeHttpHandler to support status codes

--- tests/WeatherDress.UnitTests/UnitTest1.cs
+++ tests/WeatherDress.UnitTests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace WeatherDress.UnitTests;
 public class WeatherRepositoryTests
 {
     private readonly string _todayFakeJson = "{\"hourly\":{\"time\":[\"2026-04-28T09:00\"],\"temperature_2m\":[15.0],\"weathercode\":[2],\"windspeed_10m\":[5.0],\"relativehumidity_2m\":[80],\"precipitation\":[0.0]}}";
-    private readonly string _coordinatesFakeJson = "[{\"lat\":\"55.67\",\"lon\":\"12.56\",\"display_name\":\"Copenhagen, Denmark\"}]";
+    private readonly string _coordinatesFakeJson = "{\"nr\":\"2100\",\"navn\":\"Copenhagen\",\"visueltcenter\":[12.5635,55.6786]}";
 
     [Fact]
     public void GetTodayForecast_ValidZip_ReturnsList()
@@ -24,7 +24,9 @@ public class WeatherRepositoryTests
     [Fact]
     public void GetTodayForecast_InvalidZip_ThrowsArgumentException()
     {
-        var repo = CreateRepo("[]", _todayFakeJson);
+        var responses = new Queue<string>(new[] { "" , _todayFakeJson });
+        var handler = new FakeHttpHandler(responses, firstCallStatusCode: System.Net.HttpStatusCode.NotFound);
+        var repo = new WeatherRepository(new HttpClient(handler));
 
         Assert.Throws<ArgumentException>(() => repo.GetTodayForecast("9999"));
     }
@@ -41,16 +43,20 @@ public class WeatherRepositoryTests
 public class FakeHttpHandler : HttpMessageHandler
 {
     private readonly Queue<string> _responses;
+    private readonly HttpStatusCode _firstCallStatusCode;
+    private bool _firstCall = true;
 
-    public FakeHttpHandler(Queue<string> responses)
+    public FakeHttpHandler(Queue<string> responses, HttpStatusCode firstCallStatusCode = HttpStatusCode.OK)
     {
         _responses = responses;
+        _firstCallStatusCode = firstCallStatusCode;
     }
 
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         var json = _responses.Dequeue();
-        var statusCode = json == "[]" ? HttpStatusCode.OK : HttpStatusCode.OK;
+        var statusCode = _firstCall ? _firstCallStatusCode : HttpStatusCode.OK;
+        _firstCall = false;
 
         return Task.FromResult(new HttpResponseMessage
         {
