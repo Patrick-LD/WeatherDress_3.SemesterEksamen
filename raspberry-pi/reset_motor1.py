2b4d369760e15bf010fb7b567205f1fe43e2a912
Hogsted <gustavhogsted@gmail.com>
2026-05-20 13:55:12 +0200

    fix: correct pin assignments bukser/jakke were swapped

--- raspberry-pi/reset_motor1.py
+++ raspberry-pi/reset_motor1.py
@@ -1,8 +1,8 @@
 import lgpio
 import time
 
-# BCM pins for Motor 1 Bukser (BOARD 18,22,24,26)
-PINS = [24, 25, 8, 7]
+# BCM pins for Motor 1 Bukser (BOARD 11,13,15,16)
+PINS = [17, 27, 22, 23]
 
 HALF_STEP = [
     [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
@@ -14,7 +14,7 @@ for p in PINS:
     lgpio.gpio_claim_output(h, p, 0)
 
 try:
-    for _ in range(384):
+    for _ in range(384):  # 384 = 512 - 128
         for trin in HALF_STEP:
             for pin, val in zip(PINS, trin):
                 lgpio.gpio_write(h, pin, val)
